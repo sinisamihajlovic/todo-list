40531b2ce70354dacf0a97b3f845142b2aecb5d7
Siniša Mihajlović <sinisamihajlovic@gmail.com>
2018-05-29 20:51:10 +0200

    eslint working

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ let newListItem = document.querySelector('ol');
 addItemBtn.addEventListener('click', addNewListItem);
 
 function addNewListItem() {
-    addListItemText();
-    addDelBtnToListItem();
+  addListItemText();
+  addDelBtnToListItem();
 }
 
 function addListItemText() {
-    let node = document.createElement('li');
-    let textNode = document.createTextNode('List item');
-    node.appendChild(textNode);
-    newListItem.appendChild(node);
+  let node = document.createElement('li');
+  let textNode = document.createTextNode('List item');
+  node.appendChild(textNode);
+  newListItem.appendChild(node);
 }
 
 function addDelBtnToListItem() {
-    let node = document.createElement('button');
-    let textNode = document.createTextNode('delete');
-    node.appendChild(textNode);
-    newListItem.appendChild(node);
+  let node = document.createElement('button');
+  let textNode = document.createTextNode('delete');
+  node.appendChild(textNode);
+  newListItem.appendChild(node);
 }
