552b62fcd5b43b17cdc1de4b9261eaa62392c21c
Siniša Mihajlović <sinisamihajlovic@gmail.com>
2018-06-07 18:29:23 +0200

    Clear list button added and working

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // Add item
 let addItemBtn = document.querySelector('#addItemBtn');
 let newListItem = document.querySelector('ol');
+//let items = [];
 
 addItemBtn.addEventListener('click', addNewListItem);
 
 function addNewListItem() {
   addListItemText();
   addDelBtnToListItem();
+  //items.push()
 }
 
 function addListItemText() {
@@ -18,7 +20,25 @@ function addListItemText() {
 
 function addDelBtnToListItem() {
   let node = document.createElement('button');
+
   let textNode = document.createTextNode('delete');
   node.appendChild(textNode);
   newListItem.appendChild(node);
+}
+
+// Delete item
+
+
+function deleteItem() {
+
+}
+
+// Clear list
+let clrListBtn = document.querySelector('#clearList');
+clrListBtn.addEventListener('click', clearList);
+
+function clearList() {
+  while(newListItem.firstChild) {
+    newListItem.removeChild(newListItem.firstChild);
+  }
 }
